7e86c5835568569b937c797681356ffcb2b884b2
skarn <saurabh.karn@conduent.com>
2026-05-29 16:38:40 -0500

    configured prometheus

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.schemas import TransactionRequest, FraudResponse
 from app.agent.fraud_graph import run_fraud_agent
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(title="Fraud Detection AI Agent")
 
@@ -11,3 +12,7 @@ def health():
 @app.post("/transactions/score", response_model=FraudResponse)
 def score_transaction(request: TransactionRequest):
     return run_fraud_agent(request)
+
+
+
+Instrumentator().instrument(app).expose(app)
